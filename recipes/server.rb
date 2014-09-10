392abb2e74ede5bd659566e6056bdec19b9c516c
kuroei <kuroei@gmail.com>
2014-09-10 19:52:28 +0900

    xxxx

--- recipes/server.rb
+++ recipes/server.rb
@@ -7,26 +7,36 @@ json_file_path = '/var/lib/niftycloud/automation/chef/'
 files_sort = Dir[json_file_path+"*.json"].sort_by{ |f| File.mtime(f) }
 json_file_realpath = files_sort[files_sort.length-1]
 
+p json_file_realpath
+
 json_data = open(json_file_realpath).read
 json_result = JSON.parse(json_data)
+
+#p json_result['opsworks']['layers']['zabbix']['instances'][]['ip']
 
 ZABBIX_SERVER = json_result['opsworks']['instance']['ip']
 #puts ZABBIX_SERVER
 ZABBIX_API_URL = "http://#{ZABBIX_SERVER}/api_jsonrpc.php"
-#ZABBIX_API_URL = "http://127.0.0.1/api_jsonrpc.php"
 ZABBIX_LOGINID = "admin"
 ZABBIX_PASSWORD = "zabbix"
 
-Chef::Log.info 'begin to connet to zabbix server'
-
 zbx = ZabbixApi.connect(:url => ZABBIX_API_URL, :user => ZABBIX_LOGINID, :password => ZABBIX_PASSWORD)
+#p zbx.hosts.get("output" => "extend")
+#p json_result['opsworks']['layers']['php-app']['instances'].class
 
-Chef::Log.info '[zabbix::server]:connect success'
+# need to get all agent-server before doing-json
 
 ZABBIX_HOSTS_INFO = zbx.hosts.get_full_data(:host => "")
 
+# ZABBIX_HOSTS_INFO.length
+# ZABBIX_HOSTS_INFO[0]['host']=="auto2app303"
+# ZABBIX_HOSTS_INFO[1]['host']=="auto2app303"
+#ABBIX_HOSTS_INFO.delete_if{|x| x['host']=="auto2app303"}
+# ZABBIX_HOSTS_INFO.length
+
 # this one used to get the zabbix_agent_layers
 zabbix_agent_layers = json_result['opsworks']['layers']
+#zabbix_agent_layers = "php-app"
 
 # add the all-in-one group
 if zbx.hostgroups.get_id(:name => "all-in-one")==nil
@@ -35,11 +45,9 @@ end
 
 layers_all_id = zbx.hostgroups.get_id(:name => "all-in-one")
 
-Chef::Log.info '[zabbix::server]:inited the all-in-one hostgroup'
-
 json_result['opsworks']['layers'].each do |k,v|
-  # lay_name = k
-  # if zabbix_agent_layers.include?(lay_name)
+ # lay_name = k
+ # if zabbix_agent_layers.include?(lay_name)
         # the lay is the one witch we want to add
         # if not exited
         if zbx.hostgroups.get_id(:name => k)==nil
@@ -84,18 +92,14 @@ json_result['opsworks']['layers'].each do |k,v|
                 # you need delete this server info from the ZABBIX_SERVER_INFO
                 ZABBIX_HOSTS_INFO.delete_if{|x| x['host']==php_app_host}
         end
-  # end
+  #end
 
 end
 
-#p ZABBIX_HOSTS_INFO.length
-
-Chef::Log.info '[zabbix::server]:begin to delete the Dead hosts'
+p ZABBIX_HOSTS_INFO.length
 
 ZABBIX_HOSTS_INFO.each do |x|
         zbx.hosts.delete zbx.hosts.get_id(:host => x['host'])
 end
-
-Chef::Log.info '[zabbix::server]:delete hosts success'
 
 #p zbx.hosts.get("output" => "extend")
