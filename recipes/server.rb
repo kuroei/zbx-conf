450ee2641270c71ca20613c558986251e66f08e0
kuroei <kuroei@gmail.com>
2014-09-10 11:00:07 +0900

    add hostgroup....

--- recipes/server.rb
+++ recipes/server.rb
@@ -22,28 +22,82 @@ zbx = ZabbixApi.connect(:url => ZABBIX_API_URL, :user => ZABBIX_LOGINID, :passwo
 #p zbx.hosts.get("output" => "extend")
 #p json_result['opsworks']['layers']['php-app']['instances'].class
 
-# i need to get the every servser's ip from php-app layer
-json_result['opsworks']['layers']['php-app']['instances'].each do |k,v|
+# need to get all agent-server before doing-json
 
-php_app_ip = v['private_ip']
-php_app_host = k
-#php_app_dns = v['public_dns_name']
-php_app_dns = ''
+ZABBIX_HOSTS_INFO = zbx.hosts.get_full_data(:host => "")
 
-zbx.hosts.create_or_update(
-  :host => php_app_host,
-  :interfaces => [
-    {
-      :type => 1,
-      :main => 1,
-      :ip => php_app_ip,
-      :dns => php_app_dns,
-      :port => 10050,
-      :useip => 1
-    }
-  ],
-  :groups => [ :groupid => zbx.hostgroups.get_id(:name => "Virtual machines") ]
-)
+# ZABBIX_HOSTS_INFO.length
+# ZABBIX_HOSTS_INFO[0]['host']=="auto2app303"
+# ZABBIX_HOSTS_INFO[1]['host']=="auto2app303"
+#ABBIX_HOSTS_INFO.delete_if{|x| x['host']=="auto2app303"}
+# ZABBIX_HOSTS_INFO.length
+
+# this one used to get the zabbix_agent_layers
+#zabbix_agent_layers = json_result['zabbix'][layers]
+zabbix_agent_layers = "php-app"
+
+# add the all-in-one group
+if zbx.hostgroups.get_id(:name => "all-in-one")==nil
+         zbx.hostgroups.create(:name => "all-in-one")
 end
 
-p zbx.hosts.get("output" => "extend")
+layers_all_id = zbx.hostgroups.get_id(:name => "all-in-one")
+
+json_result['opsworks']['layers'].each do |k,v|
+  lay_name = k
+  if zabbix_agent_layers.include?(lay_name)
+        # the lay is the one witch we want to add
+        # if not exited
+        if zbx.hostgroups.get_id(:name => k)==nil
+                #creat the hostgroup for this layer
+                zbx.hostgroups.create(:name => k)
+        end
+        lay_id = zbx.hostgroups.get_id(:name => k)
+        # add the server in this layer
+        v['instances'].each do |k1,v1|
+                php_app_ip = v1['private_ip']
+                php_app_host = k1
+                #php_app_dns = v1['public_dns_name']
+                php_app_dns = ''
+
+                zbx.hosts.create_or_update(
+                :host => php_app_host,
+                :interfaces => [
+                        {
+                        :type => 1,
+                        :main => 1,
+                        :ip => php_app_ip,
+                        :dns => php_app_dns,
+                        :port => 10050,
+                        :useip => 1
+                         }
+                ],
+                :groups => [
+                        {
+                        :groupid => lay_id
+                        },
+                        {
+                        :groupid => layers_all_id
+                        }
+                ]
+                )
+
+                zbx.templates.mass_add(
+                         :hosts_id => [zbx.hosts.get_id(:host => php_app_host)],
+                         :templates_id => [10050,10089]
+                )
+
+                # you need delete this server info from the ZABBIX_SERVER_INFO
+                ZABBIX_HOSTS_INFO.delete_if{|x| x['host']==php_app_host}
+        end
+  end
+
+end
+
+p ZABBIX_HOSTS_INFO.length
+
+ZABBIX_HOSTS_INFO.each do |x|
+        zbx.hosts.delete zbx.hosts.get_id(:host => x['host'])
+end
+
+#p zbx.hosts.get("output" => "extend")
